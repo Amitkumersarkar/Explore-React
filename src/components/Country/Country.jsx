5117fca89be161c16680a87e24e8d63e8c423ee1
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-02-04 00:41:30 +0600

    added updated code

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,15 @@
 import './Country.css';
 const Country = ({ country }) => {
     // console.log(country);
-    const { name, flags } = country;
+    // destructuring country
 
+    const { name, flags, population, area } = country;
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
+        <div className="country">
+            <h3>{name.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>area: {area}</p>
         </div>
     );
 };
